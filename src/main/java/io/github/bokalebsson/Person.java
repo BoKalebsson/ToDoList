d5d06d24591a545833c1a74d8af89d893d459228
BoKalebsson <bo_kalebsson@hotmail.com>
2025-07-01 15:43:41 +0200

    Added fields and constructor for Person.

--- src/main/java/io/github/bokalebsson/Person.java
+++ src/main/java/io/github/bokalebsson/Person.java
@@ -1,4 +1,19 @@
 package io.github.bokalebsson;
 
 public class Person {
+
+    // Attributes:
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    // Constructor:
+    public Person(String id, String firstName, String lastName, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
 }
