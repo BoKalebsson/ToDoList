ec464bdd8944d7afca19f1cf9dec995ae76d9c65
BoKalebsson <bo_kalebsson@hotmail.com>
2025-07-01 15:59:12 +0200

    Added getters for Person.

--- src/main/java/io/github/bokalebsson/Person.java
+++ src/main/java/io/github/bokalebsson/Person.java
@@ -1,5 +1,7 @@
 package io.github.bokalebsson;
 
+import java.util.UUID;
+
 public class Person {
 
     // Attributes:
@@ -9,11 +11,29 @@ public class Person {
     private String email;
 
     // Constructor:
-    public Person(String id, String firstName, String lastName, String email) {
-        this.id = id;
+    public Person(String firstName, String lastName, String email) {
+        this.id = UUID.randomUUID().toString();
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
     }
+
+    // Getters:
+    public String getId() {
+        return this.id;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
 
 }
