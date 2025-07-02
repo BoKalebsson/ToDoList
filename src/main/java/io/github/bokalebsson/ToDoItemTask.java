10199a80febd9df0e25b6e0bad027e60bc49dab8
BoKalebsson <bo_kalebsson@hotmail.com>
2025-07-02 13:53:26 +0200

    Added attributes for ToDoItemTask.

--- src/main/java/io/github/bokalebsson/ToDoItemTask.java
+++ src/main/java/io/github/bokalebsson/ToDoItemTask.java
@@ -1,4 +1,12 @@
 package io.github.bokalebsson;
 
 public class ToDoItemTask {
+
+    // Attributes:
+    private String id;
+    private ToDoItem toDoItem;
+    private Person assignee;
+    private boolean assigned;
+    private boolean done;
+
 }
