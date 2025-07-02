c830bf32aa8c270d37d5a22a6c903a096b47e812
BoKalebsson <bo_kalebsson@hotmail.com>
2025-07-02 11:13:37 +0200

    Added attributes for ToDoItem.

--- src/main/java/io/github/bokalebsson/ToDoItem.java
+++ src/main/java/io/github/bokalebsson/ToDoItem.java
@@ -1,4 +1,15 @@
 package io.github.bokalebsson;
 
+import java.time.LocalDate;
+
 public class ToDoItem {
+
+    // Attributes:
+    private String id;
+    private String title;
+    private String taskDescription;
+    private LocalDate deadline;
+    private boolean done;
+
+
 }
