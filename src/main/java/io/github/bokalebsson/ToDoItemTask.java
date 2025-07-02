0181996642ce50a4945cb157841b392d28c15d73
BoKalebsson <bo_kalebsson@hotmail.com>
2025-07-02 16:22:44 +0200

    Implemented ToDoItemTask attributes and constructor to meet requirements.

--- src/main/java/io/github/bokalebsson/ToDoItemTask.java
+++ src/main/java/io/github/bokalebsson/ToDoItemTask.java
@@ -1,12 +1,24 @@
 package io.github.bokalebsson;
 
+import java.util.Objects;
+
 public class ToDoItemTask {
 
+    private static int toDoItemTaskIdCounter = 0;
+
     // Attributes:
-    private String id;
+    private int id;
     private ToDoItem toDoItem;
     private Person assignee;
     private boolean assigned;
-    private boolean done;
+
+    // Constructor:
+    public ToDoItemTask(ToDoItem toDoItem, Person assignee) {
+        this.id = ++toDoItemTaskIdCounter;
+
+        this.toDoItem = Objects.requireNonNull(toDoItem, "ToDoItem cannot be null.");
+        this.assignee = assignee;
+        this.assigned = (assignee != null);
+    }
 
 }
