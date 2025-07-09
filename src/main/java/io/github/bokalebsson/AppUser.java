7e8cdb560042470d1609576c7137c292aac949a4
BoKalebsson <bo_kalebsson@hotmail.com>
2025-07-09 11:01:57 +0200

    Implemented generated default getters, setters and constructor.

--- src/main/java/io/github/bokalebsson/AppUser.java
+++ src/main/java/io/github/bokalebsson/AppUser.java
@@ -1,4 +1,42 @@
 package io.github.bokalebsson;
 
 public class AppUser {
+
+    // Attributes:
+    private String username;
+    private String password;
+    private AppRole role;
+
+    // Constructor:
+    public AppUser(String username, String password, AppRole role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    // Getters:
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public AppRole getRole() {
+        return role;
+    }
+
+    // Setters:
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRole(AppRole role) {
+        this.role = role;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
