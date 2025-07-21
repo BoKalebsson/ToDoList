1e1d30e178425346496b4ef320c4569e3cac57ad
BoKalebsson <bo_kalebsson@hotmail.com>
2025-07-21 11:35:10 +0200

    Implemented the nextId(), getCurrentId() and setCurrentId().

--- src/main/java/io/github/bokalebsson/data/sequencers/TodoItemIdSequencer.java
+++ src/main/java/io/github/bokalebsson/data/sequencers/TodoItemIdSequencer.java
@@ -1,4 +1,24 @@
 package io.github.bokalebsson.data.sequencers;
 
 public class TodoItemIdSequencer {
+
+    private static int currentId = 0;
+
+    public static int nextId(){
+        return ++currentId;
+    }
+
+    public static int getCurrentId(){
+        return currentId;
+    }
+
+    public static void setCurrentId(int id){
+        if(id < 1) {
+            throw new IllegalArgumentException("Id cannot be zero or negative.");
+        }
+        if(id < currentId) {
+            throw new IllegalArgumentException("Id cannot be less than the current ID");
+        }
+        currentId = id;
+    }
 }
