af8b7a33a664ae1d70409331c93a6b1d1ccb679a
BoKalebsson <bo_kalebsson@hotmail.com>
2025-07-21 16:20:32 +0200

    Implemented the interface with functions and comments.

--- src/main/java/io/github/bokalebsson/data/PersonDAO.java
+++ src/main/java/io/github/bokalebsson/data/PersonDAO.java
@@ -1,4 +1,42 @@
 package io.github.bokalebsson.data;
 
+import io.github.bokalebsson.Person;
+
+import java.util.Collection;
+
 public interface PersonDAO {
+
+    /**
+     * Adds a new Person to the data store.
+     * @param person the Person object to persist
+     * @return the persisted Person object, possibly with updated fields like generated id
+     */
+    Person persist(Person person);
+
+    /**
+     * Finds and returns a Person by their unique id.
+     * @param id the unique identifier of the Person
+     * @return the Person object if found, otherwise null (or optional depending on implementation)
+     */
+    Person findById(int id);
+
+    /**
+     * Finds and returns a Person by their unique email address.
+     * @param email the email address of the Person
+     * @return the Person object if found, otherwise null
+     */
+    Person findByEmail(String email);
+
+    /**
+     * Returns all Person objects currently stored.
+     * @return a collection containing all Persons
+     */
+    Collection<Person> findAll();
+
+    /**
+     * Removes a Person from the data store by their unique id.
+     * @param id the unique identifier of the Person to remove
+     */
+    void remove(int id);
+
 }
