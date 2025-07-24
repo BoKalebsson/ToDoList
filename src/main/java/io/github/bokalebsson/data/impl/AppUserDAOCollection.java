e4372a8c8a812f2dbc9c8945d1cc3604914aa9c9
BoKalebsson <bo_kalebsson@hotmail.com>
2025-07-24 10:39:07 +0200

    Implemented empty methods.

--- src/main/java/io/github/bokalebsson/data/impl/AppUserDAOCollection.java
+++ src/main/java/io/github/bokalebsson/data/impl/AppUserDAOCollection.java
@@ -1,4 +1,30 @@
 package io.github.bokalebsson.data.impl;
 
-public class AppUserDAOCollection {
+import io.github.bokalebsson.AppUser;
+import io.github.bokalebsson.data.AppUserDAO;
+
+import java.util.Collection;
+import java.util.List;
+
+public class AppUserDAOCollection implements AppUserDAO {
+
+    @Override
+    public AppUser persist(AppUser appUser) {
+        return null;
+    }
+
+    @Override
+    public AppUser findByUsername(String username) {
+        return null;
+    }
+
+    @Override
+    public Collection<AppUser> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public void remove(String username) {
+
+    }
 }
