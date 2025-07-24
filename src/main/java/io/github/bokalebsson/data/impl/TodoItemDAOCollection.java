85db8bfe4c34d8f2beb04f4bbe7e93e3f1a9e367
BoKalebsson <bo_kalebsson@hotmail.com>
2025-07-24 13:58:10 +0200

    Implemented empty methods().

--- src/main/java/io/github/bokalebsson/data/impl/TodoItemDAOCollection.java
+++ src/main/java/io/github/bokalebsson/data/impl/TodoItemDAOCollection.java
@@ -1,4 +1,56 @@
 package io.github.bokalebsson.data.impl;
 
-public class TodoItemDAOCollection {
+import io.github.bokalebsson.ToDoItem;
+import io.github.bokalebsson.data.ToDoItemDAO;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
+
+public class TodoItemDAOCollection implements ToDoItemDAO {
+
+    @Override
+    public ToDoItem persist(ToDoItem toDoItem) {
+        return null;
+    }
+
+    @Override
+    public ToDoItem findById(int id) {
+        return null;
+    }
+
+    @Override
+    public Collection<ToDoItem> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public Collection<ToDoItem> findAllByDoneStatus(boolean done) {
+        return List.of();
+    }
+
+    @Override
+    public Collection<ToDoItem> findByTitleContains(String title) {
+        return List.of();
+    }
+
+    @Override
+    public Collection<ToDoItem> findByPersonId(int personId) {
+        return List.of();
+    }
+
+    @Override
+    public Collection<ToDoItem> findByDeadlineBefore(LocalDate date) {
+        return List.of();
+    }
+
+    @Override
+    public Collection<ToDoItem> findByDeadlineAfter(LocalDate date) {
+        return List.of();
+    }
+
+    @Override
+    public void remove(int id) {
+
+    }
 }
