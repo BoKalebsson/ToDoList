b63a11b6b1d68736e819886ebcd8147fdc92b6e6
BoKalebsson <bo_kalebsson@hotmail.com>
2025-07-25 11:36:43 +0200

    Implemented empty methods.

--- src/main/java/io/github/bokalebsson/data/impl/TodoItemTaskDAOCollection.java
+++ src/main/java/io/github/bokalebsson/data/impl/TodoItemTaskDAOCollection.java
@@ -1,4 +1,39 @@
 package io.github.bokalebsson.data.impl;
 
-public class TodoItemTaskDAOCollection {
+import io.github.bokalebsson.ToDoItemTask;
+import io.github.bokalebsson.data.ToDoItemTaskDAO;
+
+import java.util.Collection;
+import java.util.List;
+
+public class TodoItemTaskDAOCollection implements ToDoItemTaskDAO {
+    @Override
+    public ToDoItemTask persist(ToDoItemTask toDoItemTask) {
+        return null;
+    }
+
+    @Override
+    public ToDoItemTask findById(int id) {
+        return null;
+    }
+
+    @Override
+    public Collection<ToDoItemTask> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public Collection<ToDoItemTask> findByAssignedStatus(boolean assigned) {
+        return List.of();
+    }
+
+    @Override
+    public Collection<ToDoItemTask> findByPersonId(int personId) {
+        return List.of();
+    }
+
+    @Override
+    public void remove(int id) {
+
+    }
 }
