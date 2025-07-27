b26e67f6b309c3d19c93a0ebb1b1a2e8574ff38e
BoKalebsson <bo_kalebsson@hotmail.com>
2025-07-27 17:59:37 +0200

    Implemented saveListToFile()-method.

--- src/main/java/io/github/bokalebsson/data/util/FileStorageManager.java
+++ src/main/java/io/github/bokalebsson/data/util/FileStorageManager.java
@@ -1,4 +1,21 @@
 package io.github.bokalebsson.data.util;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
 public class FileStorageManager {
+
+    public <T> void saveListToFile(List<T> list, File file, Class<T> type) throws IOException {
+        if (list == null) throw new IllegalArgumentException("List is null");
+        if (file == null) throw new IllegalArgumentException("File is null");
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(file, list);
+    }
+
 }
+
+
