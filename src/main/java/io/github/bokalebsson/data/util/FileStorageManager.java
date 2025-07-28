919fbcb5877065d39781f2d0a41a3583d2d01e58
BoKalebsson <bo_kalebsson@hotmail.com>
2025-07-28 16:31:50 +0200

    Implemented support for reading from / writing to Maps.

--- src/main/java/io/github/bokalebsson/data/util/FileStorageManager.java
+++ src/main/java/io/github/bokalebsson/data/util/FileStorageManager.java
@@ -1,5 +1,6 @@
 package io.github.bokalebsson.data.util;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
@@ -9,6 +10,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 public class FileStorageManager {
 
@@ -34,6 +36,28 @@ public class FileStorageManager {
         JavaType listType = mapper.getTypeFactory().constructCollectionType(List.class, type);
 
         return mapper.readValue(file, listType);
+    }
+
+    public <K, V> void saveMapToFile(Map<K, V> map, File file) throws IOException {
+        if (map == null) throw new IllegalArgumentException("Map is null");
+        if (file == null) throw new IllegalArgumentException("File is null");
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        mapper.writeValue(file, map);
+    }
+
+    public <K, V> Map<K, V> loadMapFromFile(File file, TypeReference<Map<K, V>> typeRef) throws IOException {
+        if (file == null) throw new IllegalArgumentException("File is null");
+        if (!file.exists()) throw new FileNotFoundException("File does not exist: " + file.getAbsolutePath());
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        return mapper.readValue(file, typeRef);
     }
 
 }
