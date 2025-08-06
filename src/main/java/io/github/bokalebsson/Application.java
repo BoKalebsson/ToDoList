dd63fb9cf645ac24c83451172654d20ec32e6068
BoKalebsson <bo_kalebsson@hotmail.com>
2025-08-06 13:46:04 +0200

    Cleaned up main to be run from Application.java instead.

--- src/main/java/io/github/bokalebsson/Application.java
+++ src/main/java/io/github/bokalebsson/Application.java
@@ -1,4 +1,96 @@
 package io.github.bokalebsson;
 
+import io.github.bokalebsson.dao.impl.AppUserDAOCollection;
+import io.github.bokalebsson.dao.impl.PersonDAOCollection;
+import io.github.bokalebsson.dao.impl.ToDoItemDAOCollection;
+import io.github.bokalebsson.dao.impl.ToDoItemTaskDAOCollection;
+import io.github.bokalebsson.util.ApplicationDataManager;
+import io.github.bokalebsson.model.*;
+
+import java.time.LocalDate;
+
 public class Application {
+
+    private final String dataFolderPath;
+
+    private PersonDAOCollection personDAO;
+    private AppUserDAOCollection appUserDAO;
+    private ToDoItemDAOCollection toDoItemDAO;
+    private ToDoItemTaskDAOCollection toDoItemTaskDAO;
+    private ApplicationDataManager applicationDataManager;
+
+    // Constructor for filepathing to JSON and properties-files:
+    public Application(String dataFolderPath) {
+        this.dataFolderPath = dataFolderPath;
+    }
+
+    public void start() {
+        loadData();
+        workArea();
+        saveData();
+        System.out.println("Application closed. Data saved.");
+    }
+
+    private void loadData() {
+        // Create DAO collections:
+        personDAO = new PersonDAOCollection();
+        toDoItemDAO = new ToDoItemDAOCollection();
+        toDoItemTaskDAO = new ToDoItemTaskDAOCollection();
+        appUserDAO = new AppUserDAOCollection();
+
+        // Create and load Application Data Manager:
+        applicationDataManager = new ApplicationDataManager(
+                dataFolderPath, personDAO, appUserDAO, toDoItemDAO, toDoItemTaskDAO);
+
+        applicationDataManager.loadData();
+    }
+
+    private void workArea() {
+
+        // Create a new AppUser:
+        AppUser user = new AppUser("Erik", "123456", AppRole.ROLE_APP_ADMIN);
+        appUserDAO.persist(user);
+        System.out.println(user);
+
+        // Create another AppUser:
+        AppUser user2 = new AppUser("Anna", "654321", AppRole.ROLE_APP_USER);
+        appUserDAO.persist(user2);
+        System.out.println(user2);
+
+        // Create a new Person:
+        Person erik = new Person("Erik", "Andersson", "erik@test.nu", user);
+        personDAO.persist(erik);
+        System.out.println(erik);
+
+        // Create another Person:
+        Person anna = new Person("Anna", "Svensson", "anna@test.nu", user2);
+        personDAO.persist(anna);
+        System.out.println(anna);
+
+        // Create a new ToDoItem:
+        ToDoItem item = new ToDoItem("Grocery shopping!",
+                "Buy eggs and milk.", LocalDate.now().plusDays(7), erik);
+        toDoItemDAO.persist(item);
+        System.out.println(item);
+
+        // Create another ToDoItem:
+        ToDoItem item2 = new ToDoItem("Study Java!",
+                "Finish the assignment!", LocalDate.now().plusDays(7), anna);
+        toDoItemDAO.persist(item2);
+        System.out.println(item2);
+
+        // Create a new ToDoItemTask:
+        ToDoItemTask task = new ToDoItemTask(item, erik);
+        toDoItemTaskDAO.persist(task);
+        System.out.println(task);
+
+        // Create another ToDoItemTask:
+        ToDoItemTask task2 = new ToDoItemTask(item2, anna);
+        toDoItemTaskDAO.persist(task2);
+        System.out.println(task2);
+    }
+
+    private void saveData() {
+        applicationDataManager.saveData();
+    }
 }
--- src/main/java/io/github/bokalebsson/Main.java
+++ src/main/java/io/github/bokalebsson/Main.java
@@ -12,76 +12,8 @@ import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-
-        // 1. STARTUP – Read data
-
-        // Create filepath:
-        String dataFolderPath = "data";
-
-        // Create DAO collections:
-        PersonDAOCollection personDAO = new PersonDAOCollection();
-        ToDoItemDAOCollection toDoItemDAO = new ToDoItemDAOCollection();
-        ToDoItemTaskDAOCollection toDoItemTaskDAO = new ToDoItemTaskDAOCollection();
-        AppUserDAOCollection appUserDAO = new AppUserDAOCollection();
-
-        // Create and load Application Data Manager:
-        ApplicationDataManager applicationDataManager = new ApplicationDataManager(
-                dataFolderPath, personDAO, appUserDAO, toDoItemDAO, toDoItemTaskDAO
-        );
-
-        // Load data from JSON-files and Sequencers:
-        applicationDataManager.loadData();
-
-        // 2. WORK AREA – Manipulate data:
-
-        // Create a new AppUser:
-        AppUser user = new AppUser("Erik", "123456", AppRole.ROLE_APP_ADMIN);
-        appUserDAO.persist(user);
-        System.out.println(user);
-
-        // Create another AppUser:
-        AppUser user2 = new AppUser("Anna", "654321", AppRole.ROLE_APP_USER);
-        appUserDAO.persist(user2);
-        System.out.println(user2);
-
-        // Create a new Person:
-        Person erik = new Person("Erik", "Andersson", "erik@test.nu", user);
-        personDAO.persist(erik);
-        System.out.println(erik);
-
-        // Create another AppUser:
-        Person anna = new Person("Anna", "Svensson", "anna@test.nu", user2);
-        personDAO.persist(anna);
-        System.out.println(anna);
-
-        // Create a new ToDoItem:
-        ToDoItem item = new ToDoItem("Grocery shopping!",
-                "Buy eggs and milk.", LocalDate.now().plusDays(7), erik);
-        toDoItemDAO.persist(item);
-        System.out.println(item);
-
-        // Create another ToDoItem:
-        ToDoItem item2 = new ToDoItem("Study Java!",
-                "Finish the assignment!", LocalDate.now().plusDays(7), anna);
-        toDoItemDAO.persist(item2);
-        System.out.println(item2);
-
-        // Create a new ToDoItemTask:
-        ToDoItemTask task = new ToDoItemTask(item, erik);
-        toDoItemTaskDAO.persist(task);
-        System.out.println(task);
-
-        // Create another ToDoItemTask:
-        ToDoItemTask task2 = new ToDoItemTask(item2, anna);
-        toDoItemTaskDAO.persist(task2);
-        System.out.println(task2);
-
-        // 3. SHUTDOWN – Save data to files:
-
-        // Save data from JSON-files and Sequencers:
-        applicationDataManager.saveData();
-
-
+        Application app = new Application("data");
+        app.start();
     }
 
 }
