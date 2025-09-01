8a806f6f5069e63a92f8e28c30722a3c2ec58972
BoKalebsson <bo_kalebsson@hotmail.com>
2025-09-01 15:14:42 +0200

    Setting up a clean main for testing.

--- src/main/java/io/github/bokalebsson/Main.java
+++ src/main/java/io/github/bokalebsson/Main.java
@@ -1,19 +1,8 @@
 package io.github.bokalebsson;
 
-import io.github.bokalebsson.dao.impl.AppUserDAOCollection;
-import io.github.bokalebsson.dao.impl.PersonDAOCollection;
-import io.github.bokalebsson.dao.impl.ToDoItemDAOCollection;
-import io.github.bokalebsson.dao.impl.ToDoItemTaskDAOCollection;
-import io.github.bokalebsson.util.ApplicationDataManager;
-import io.github.bokalebsson.model.*;
-
-import java.time.LocalDate;
-
-
 public class Main {
     public static void main(String[] args) {
-        Application app = new Application("data");
-        app.start();
+
     }
 
 }
