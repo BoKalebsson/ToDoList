4d091051ae641663ca454d44a070d6ac60c4a238
BoKalebsson <bo_kalebsson@hotmail.com>
2025-09-01 15:32:48 +0200

    Established a connection successfully.

--- src/main/java/io/github/bokalebsson/Main.java
+++ src/main/java/io/github/bokalebsson/Main.java
@@ -1,7 +1,18 @@
 package io.github.bokalebsson;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Main {
     public static void main(String[] args) {
+
+        // Establish a connection:
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/todoit", "root", "root");
+            System.out.println("Connection successful!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
