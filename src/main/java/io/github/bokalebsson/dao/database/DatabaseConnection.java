5c075624fce1ce7ad279c216d7c70bd6a2d4046c
BoKalebsson <bo_kalebsson@hotmail.com>
2025-09-02 15:40:25 +0200

    Implemented an interface to handle the connections to databases.

--- src/main/java/io/github/bokalebsson/dao/database/DatabaseConnection.java
+++ src/main/java/io/github/bokalebsson/dao/database/DatabaseConnection.java
@@ -1,4 +1,19 @@
 package io.github.bokalebsson.dao.database;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public interface DatabaseConnection {
+
+    /**
+     * Returns a JDBC Connection to the database.
+     *
+     * <p>Classes that access the database should use this method
+     * without worrying about how the connection is configured.</p>
+     *
+     * @return a new {@link Connection}
+     * @throws SQLException if a database access error occurs
+     */
+    Connection getConnection() throws SQLException;
+
 }
