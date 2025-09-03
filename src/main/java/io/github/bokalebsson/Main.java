cceab38327bf9affe923527ef1e959018615d045
BoKalebsson <bo_kalebsson@hotmail.com>
2025-09-03 16:10:49 +0200

    Tested findAll()-method.

--- src/main/java/io/github/bokalebsson/Main.java
+++ src/main/java/io/github/bokalebsson/Main.java
@@ -1,45 +1,29 @@
 package io.github.bokalebsson;
 
-import io.github.bokalebsson.dao.connections.DatabaseConnection;
-import io.github.bokalebsson.dao.connections.MySQLDatabaseConnection;
+import io.github.bokalebsson.dao.database.DBPerson;
+import io.github.bokalebsson.dao.database.People;
+import io.github.bokalebsson.dao.database.PeopleDAO;
 
 import java.sql.*;
+import java.util.Collection;
 
 public class Main {
     public static void main(String[] args) {
 
-        // Create a new MySQL-connection:
-        DatabaseConnection databaseConnection = new MySQLDatabaseConnection();
+        // Create a PeopleDAO instance:
+        People peopleDao = new PeopleDAO();
 
-        // Establish a connection to the database:
-        try (Connection connection = databaseConnection.getConnection()){
-            System.out.println("✅ Connection successful!");
+        // Call findAll() to fetch all persons from the database:
+        Collection<DBPerson> persons = peopleDao.findAll();
 
-            /* This section will be used to test methods from the database-classes. */
-
-            // Prepare a statement:
-            PreparedStatement ps = connection.prepareStatement("SELECT * FROM person WHERE first_name = ?");
-            ps.setString(1, "Erik");
-
-            // Run the query:
-            ResultSet resultSet = ps.executeQuery();
-
-            // Print the result:
-            while (resultSet.next()){
-
-                int personId = resultSet.getInt("person_id");
-                String firstName = resultSet.getString("first_name");
-                String lastName = resultSet.getString("last_name");
-
-                System.out.println("Id: " + personId + " | " + "Name: " + firstName + " " + lastName);
-
+        // Print the results:
+        if (persons.isEmpty()) {
+            System.out.println("⚠️ No persons found in the database.");
+        } else {
+            System.out.println("✅ Found " + persons.size() + " persons:");
+            for (DBPerson p : persons) {
+                System.out.println(p);
             }
-
-        } catch (SQLException e){
-            System.err.println("❌ Failed to connect to the database:");
-            System.err.println("Error message: " + e.getMessage());
-            System.err.println("SQL state: " + e.getSQLState());
-            System.err.println("Error code: " + e.getErrorCode());
         }
 
     }
