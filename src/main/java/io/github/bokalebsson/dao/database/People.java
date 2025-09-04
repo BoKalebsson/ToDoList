10a402c83d5b2469160d5f0f3577e8942e42a54b
BoKalebsson <bo_kalebsson@hotmail.com>
2025-09-04 17:40:39 +0200

    Refactored interface to throws SQLException.

--- src/main/java/io/github/bokalebsson/dao/database/People.java
+++ src/main/java/io/github/bokalebsson/dao/database/People.java
@@ -1,19 +1,20 @@
 package io.github.bokalebsson.dao.database;
 
+import java.sql.SQLException;
 import java.util.Collection;
 
 public interface People {
 
-    DBPerson create(DBPerson dbPerson);
+    DBPerson create(DBPerson dbPerson) throws SQLException;
 
-    Collection<DBPerson> findAll();
+    Collection<DBPerson> findAll() throws SQLException;
 
-     DBPerson findById(int id);
+     DBPerson findById(int id) throws SQLException;
 
-    Collection<DBPerson> findByName(String name);
+    Collection<DBPerson> findByName(String name) throws SQLException;
 
-    DBPerson update(DBPerson dbPerson);
+    DBPerson update(DBPerson dbPerson) throws SQLException;
 
-    boolean deleteById(int id);
+    boolean deleteById(int id) throws SQLException;
 
 }
