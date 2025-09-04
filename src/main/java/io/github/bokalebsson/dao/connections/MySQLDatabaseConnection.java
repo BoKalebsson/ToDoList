e8ed8515e4b55fce4a42e28f76ffa3c82acfd3c2
BoKalebsson <bo_kalebsson@hotmail.com>
2025-09-04 16:52:42 +0200

    Implemented a connection using enviroment variables added in IntelliJ.

--- src/main/java/io/github/bokalebsson/dao/connections/MySQLDatabaseConnection.java
+++ src/main/java/io/github/bokalebsson/dao/connections/MySQLDatabaseConnection.java
@@ -4,9 +4,9 @@ import java.sql.*;
 
 public class MySQLDatabaseConnection implements DatabaseConnection {
 
-    private final String URL = "jdbc:mysql://localhost:3306/todoit";
-    private final String USER = "root";
-    private final String PASSWORD = "root";
+    private final String URL = System.getenv("DB_URL");
+    private final String USER = System.getenv("DB_USER");
+    private final String PASSWORD = System.getenv("DB_PASSWORD");
 
     @Override
     public Connection getConnection() throws SQLException {
