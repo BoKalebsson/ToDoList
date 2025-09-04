23ddfd924c90951e4ccc1de61d12f81bf8b3810d
BoKalebsson <bo_kalebsson@hotmail.com>
2025-09-04 17:28:22 +0200

    Implemented the interface for ToDoItems.

--- src/main/java/io/github/bokalebsson/dao/database/ToDoItems.java
+++ src/main/java/io/github/bokalebsson/dao/database/ToDoItems.java
@@ -1,4 +1,26 @@
 package io.github.bokalebsson.dao.database;
 
+import java.sql.SQLException;
+import java.util.Collection;
+
 public interface ToDoItems {
+
+    DBTodo create(DBTodo dbTodo) throws SQLException;
+
+    Collection<DBTodo> findAll() throws SQLException;
+
+    DBTodo findById(int id) throws SQLException;
+
+    Collection<DBTodo> findByDoneStatus(boolean isDone) throws SQLException;
+
+    Collection<DBTodo> findByAssignee(int assigneeId) throws SQLException;
+
+    Collection<DBTodo> findByAssignee(DBPerson dbPerson) throws SQLException;
+
+    Collection<DBTodo> findByUnassignedToDoItems() throws SQLException;
+
+    DBTodo update(DBTodo dbTodo) throws SQLException;
+
+    boolean deleteById(int id) throws SQLException;
+
 }
