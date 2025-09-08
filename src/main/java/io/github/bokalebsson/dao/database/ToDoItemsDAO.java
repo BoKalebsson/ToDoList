15eb85993661ffb73d52d46eec37d7ab629a36b4
BoKalebsson <bo_kalebsson@hotmail.com>
2025-09-08 10:10:38 +0200

    Implemented create()-method.

--- src/main/java/io/github/bokalebsson/dao/database/ToDoItemsDAO.java
+++ src/main/java/io/github/bokalebsson/dao/database/ToDoItemsDAO.java
@@ -1,14 +1,51 @@
 package io.github.bokalebsson.dao.database;
 
-import java.sql.SQLException;
+import io.github.bokalebsson.dao.connections.DatabaseConnection;
+import io.github.bokalebsson.dao.connections.MySQLDatabaseConnection;
+
+import java.sql.*;
 import java.util.Collection;
 import java.util.List;
 
 public class ToDoItemsDAO implements ToDoItems {
 
+    private final DatabaseConnection databaseConnection = new MySQLDatabaseConnection();
+
     @Override
     public DBTodo create(DBTodo dbTodo) throws SQLException {
-        return null;
+        String sql = "INSERT INTO todo_item(title, description, deadline, done, assignee_id) VALUES (?, ?, ?, ?, ?) ";
+
+        try (Connection connection = databaseConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)
+        ) {
+
+            preparedStatement.setString(1, dbTodo.getTitle());
+            preparedStatement.setString(2, dbTodo.getDescription());
+            preparedStatement.setDate(3, Date.valueOf(dbTodo.getDeadline()));
+            preparedStatement.setBoolean(4, dbTodo.isDone());
+            preparedStatement.setInt(5, dbTodo.getAssigneeId());
+
+            int affectedRows = preparedStatement.executeUpdate();
+
+            if (affectedRows == 0) {
+                throw new SQLException("⚠️ Creating todo failed, no rows affected.");
+            }
+
+            try (ResultSet generatedKeys = preparedStatement.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    int generatedId = generatedKeys.getInt(1);
+                    return new DBTodo(
+                            generatedId,
+                            dbTodo.getTitle(),
+                            dbTodo.getDescription(),
+                            dbTodo.getDeadline(),
+                            dbTodo.isDone(),
+                            dbTodo.getAssigneeId());
+                } else {
+                    throw new SQLException("⚠️ Creating todo failed, no ID obtained.");
+                }
+            }
+        }
     }
 
     @Override
