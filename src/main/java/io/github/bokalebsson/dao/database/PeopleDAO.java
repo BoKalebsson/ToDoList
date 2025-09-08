0189b66c6bf4c90ca45d8a4942919d7d14136ff3
BoKalebsson <bo_kalebsson@hotmail.com>
2025-09-08 09:37:00 +0200

    Completed the refactoring to make methods throw SQLExceptions.

--- src/main/java/io/github/bokalebsson/dao/database/PeopleDAO.java
+++ src/main/java/io/github/bokalebsson/dao/database/PeopleDAO.java
@@ -14,9 +14,6 @@ public class PeopleDAO implements People {
     private final DatabaseConnection databaseConnection = new MySQLDatabaseConnection();
 
 
-    //TODO: Refactor the code to throw SQL exceptions, and let other classes and/or methods handle the error handling
-    // to inform the user that something went wrong.
-
     @Override
     public DBPerson create(DBPerson dbPerson) throws SQLException {
         String sql = "INSERT INTO person (first_name, last_name) VALUES (?, ?)";
